8cf86e2e89ecc5a299d7556d7a128a2c1dd3cb48
Safiahmad Shikalgar <safishikalgar@gmail.com>
2026-09-23 20:13:09 +0530

    Prepare SMRITI-NER Flutter web deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 import 'app.dart';
 import 'data/database/app_database.dart';
@@ -9,6 +12,10 @@ import 'services/tts/tts_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
 
   // 1. Initialize SQLite local database and seed demo data on first launch
   try {
